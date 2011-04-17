052577cdccd898cfd5817aff3d388caa8781c286
Franklin Webber <franklin.webber@gmail.com>
2011-04-16 23:26:46 -0700

    CSS, JS, and search fields
      Updated to be a little more clear what is taking place. To allow for
      multiple plugins to play nicely layout/html/setup.rb is calling super on
      the javascript, stylesheets, and search fields. If they do not exist then
      this is the only plugin agreeing to this convention and we should default
      to the existing css, js, and search fields

--- lib/templates/default/layout/html/setup.rb
+++ lib/templates/default/layout/html/setup.rb
@@ -2,12 +2,31 @@ def init
   super
 end
 
-def javascript_files
-  existing_js = super rescue [ 'js/jquery.js','js/app.js' ]
-  existing_js + [ 'js/rspec.js' ]
+def yard_default_stylesheets
+  [ 'css/style.css', 'css/common.css' ]
+end
+
+def yard_default_javascripts
+  [ 'js/jquery.js','js/app.js' ]
+end
+
+def yard_default_search_fields
+  [ { :type => 'class', :title => 'Classes', :search_title => 'Class List' },
+    { :type => 'method', :title => 'Methods', :search_title => 'Method List' }, 
+    { :type => 'file', :title => 'Files', :search_title => 'File List' } ]
+end
+
+def stylesheets
+  css = super rescue yard_default_stylesheets
+  css + [ 'css/rspec.css' ]
+end
+
+def javascripts
+  js = super rescue yard_default_javascripts
+  js + [ 'js/rspec.js' ]
 end
 
 def search_fields
-  existing_fields = super rescue [ [ 'class_list_link', 'Class List' ],[ 'method_list_link', 'Method List' ],[ 'file_list_link', 'File List' ] ] 
-  existing_fields + [ [ 'specs_list_link', 'Specs' ] ]
+  fields = super rescue yard_default_search_fields
+  [ { :type => 'spec', :title > 'Specs', :search_title => 'Specs' } ] + fields
 end
