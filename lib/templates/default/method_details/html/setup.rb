338f71cc9bc59977dd8e0d2d950f48b56e90cc2f
Franklin Webber <franklin.webber@gmail.com>
2011-04-26 19:06:15 -0700

    Class and Method Specification Link to Full Spec
      + class specifications link to the full spec
      + method specifications, find the topmost parent, and link to the full spec

--- lib/templates/default/method_details/html/setup.rb
+++ lib/templates/default/method_details/html/setup.rb
@@ -6,3 +6,13 @@ end
 def contexts
   @contexts ||= ( object[:specifications] ? object[:specifications].uniq : nil )
 end
+
+def link_to_full_specification(spec)
+  context = spec.parent
+  
+  until context.parent == YARD::CodeObjects::RSpec::RSPEC_NAMESPACE
+    context = context.parent
+  end
+  
+  linkify context, spec.value
+end
