21f04299032e1738afb178c4da44e0831555a4f8
Franklin Webber <franklin.webber@gmail.com>
2011-04-26 19:25:51 -0700

    Linked sub-context to the paired method
      + created a link from the context value to the class/module method
      + changed the css to make the pointer the entire bar
      + changed the js to make the area to expand the entire bar

--- lib/templates/default/context/html/setup.rb
+++ lib/templates/default/context/html/setup.rb
@@ -26,3 +26,14 @@ end
 def row
   @row
 end
+
+# If the context is linked to code object (e.g. class, module, method) then
+# create a link back to that object, otherwise just display the value of the
+# context
+def link_to_code_object(context)
+  if context.paired_to_code_object
+    linkify context.paired_to_code_object, h(context.value)
+  else
+    h(context.value)
+  end
+end
