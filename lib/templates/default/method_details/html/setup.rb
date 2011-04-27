7e6ccb9a9322e0a3c0bd9ba5ed4f35fbeb54d977
Franklin Webber <franklin.webber@gmail.com>
2011-04-26 18:41:16 -0700

    Specifications in Method Template
      Added the specs back to the method template, now that they are linked.
      However, they appear twice in the list

--- lib/templates/default/method_details/html/setup.rb
+++ lib/templates/default/method_details/html/setup.rb
@@ -4,7 +4,6 @@ def init
 end
 
 def contexts
-  #log.debug "method_details - Finding Contexts"
-  #@contexts ||= object.children.find_all {|child| child.is_a?(YARD::CodeObjects::RSpec::Context)}
-  nil
+  log.debug "method_details - Finding Contexts"
+  @contexts ||= object[:specifications]
 end
