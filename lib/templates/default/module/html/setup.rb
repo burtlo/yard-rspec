103118d5fd9f6a848131fe9056ae8666e782ede3
Franklin Webber <franklin.webber@gmail.com>
2011-05-03 17:41:40 -0700

    RSpec Failures Inline
      Failures appear inline with the same poor logic duplicated

--- lib/templates/default/module/html/setup.rb
+++ lib/templates/default/module/html/setup.rb
@@ -14,3 +14,22 @@ end
 def link_to_full_specification(context,spec)
   %{<a href='#{url_for(context,spec.unique_id)}'>#{h(spec.value)}</a>}
 end
+
+#
+# Return a failure to the specification
+# 
+def failure(spec)
+  
+  # Based on the spec we need to see if there is
+  # a failure that is associated with it. This can
+  # be done by examining each failure through its
+  # context tree and the specification through it's
+  # context tree. Or we could compare values of the 
+  # complete statement
+  failures = YARD::CodeObjects::RSpec::RSPEC_NAMESPACE.children.find_all {|child| child.is_a? YARD::CodeObjects::RSpec::Failure }
+  
+  failures.find do |fail| 
+    fail.value == spec.full_name
+  end
+  
+end
