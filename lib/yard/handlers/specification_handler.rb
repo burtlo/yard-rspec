8572301a64b1e77f48e99cd256a7dfa96c621fff
Franklin Webber <franklin.webber@gmail.com>
2011-06-10 00:45:40 -0700

    FIX: Inline specifications
      - inline specifications would generate and error because they
        did not have the field that was being requested. Changed over
        to requesting the children of the statements owner which appears
        to work for both inline and traditional block specs

--- lib/yard/handlers/specification_handler.rb
+++ lib/yard/handlers/specification_handler.rb
@@ -3,8 +3,21 @@ class RSpecSpecificationHandler < YARD::Handlers::Ruby::Base
   handles method_call(:it)
   
   def process
+    # The statements first child is the "it"
+    # The second child is the name of the specification, when that is not present
+    #     then we are likely dealing with an anonymous inline spec.
+    # The third child is the block of the specification
+
+    
     # TODO: concerned about the same named specifications
-    name = statement.parameters.first.jump(:string_content).source
+    
+    name = statement.children[1].source
+    
+    # When the name of the specification is an empty string we will use the
+    # contents of block to represent what the specification is attempting to 
+    # describe about the context
+
+    name = statement.last.last.source.chomp if name == ""
     
     if owner.is_a?(YARD::CodeObjects::RSpec::Context) or
        owner.is_a?(YARD::CodeObjects::RSpec::SharedExample)
